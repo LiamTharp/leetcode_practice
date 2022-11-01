7bab7642723c0e54db85057d2c302cc0dd4844f6
liam <liamtharp@gmail.com>
2022-10-31 22:55:39 -0400

    height_balanced: fixing to not return global variable.

--- height_balanced.py
+++ height_balanced.py
@@ -1,26 +1,25 @@
-from typing import Optional 
+from typing import Optional
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
-
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
-        max = self.dfs(root, function = max, height = 0)
 
-        min = self.dfs(root.right, function = min, height = 0)
+        return self.dfs(root) != -1
 
-    
-    def dfs(self, function, root, height = 0) -> int:
-        
-        if root:
-            left = self.dfs(root.left)
-            right = self.dfs(root.right)
+    def dfs(self, root) -> int:
 
-            height += function(left, right) + 1
+        if root is None: return 0
+            
+        l = self.dfs(root.left)
+        r = self.dfs(root.right)
+
+        if l == -1 or r == -1 or abs(l-r) > 1:
+            return -1
         
-        return height
+        return max(l,r) + 1
+        
